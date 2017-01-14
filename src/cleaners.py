7f429a5d3379a1c3e32f2019138dfaf62783543a
lromang <luis.roangarci@gmail.com>
2017-01-14 13:08:32 -0600

    cleaers

---
+++ src/cleaners.py
@@ -0,0 +1,16 @@
+#! /bin/python
+
+'''
+------------------------------
+# Authors:
+  - Miguel Vilchis
+  - Luis Román
+------------------------------
+# Description:
+This script contains multiple utileries
+for cleaning and processing datasets.
+------------------------------
+# Usage:
+
+
+'''
