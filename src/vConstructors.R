6f1554361f5ecf60683b330871587f2e04899457
lromang <luis.roangarci@gmail.com>
2017-01-18 08:13:56 -0600

    const

---
+++ src/vConstructors.R
@@ -0,0 +1,43 @@
+#! /bin/python
+
+
+## ------------------------------
+# Authors:
+##  - Miguel Vilchis
+##  - Luis Román
+## ------------------------------
+## Description:
+## This script contains multiple utileries
+## for building the general prospects
+## and institutions tables.
+## ------------------------------
+# Usage:
+
+
+##
+
+## ------------------------------
+## Libraries
+## ------------------------------
+library(data.table)
+
+## ------------------------------
+## Functions
+## ------------------------------
+
+get_clients <- function(clients, invoices){
+    n_clients      <- clients[, .N, by = IdTaxEntity]
+    amount_clients <- invoices[, sum(Currency), ]
+}
+
+get_providers <- function(providers, invoices){
+    clients[, .N, by = IdTaxEntity]
+}
+
+
+## ------------------------------
+## Read in data
+## ------------------------------
+invoices   <- fread('../data/clean_data/df_invoices.csv')
+providers  <- fread('../data/export_V_Providers.csv')
+clients    <- fread('../data/export_v_clients.csv')
