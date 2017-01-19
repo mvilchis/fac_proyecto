c2f2c62a75f3f439c2194f33b103c8e4c079c9c1
lromang <luis.roangarci@gmail.com>
2017-01-18 18:19:54 -0600

    income variables

--- src/vConstructors.R
+++ src/vConstructors.R
@@ -23,21 +23,58 @@ library(data.table)
 
 ## ------------------------------
 ## Functions
+##
+## ------------------------------
+## Get max mean income:
+## (could be max sum)
+## - Per client
+## - Per day
+## - Per Month
+## - Per year
 ## ------------------------------
 
-get_clients <- function(clients, invoices){
-    n_clients      <- clients[, .N, by = IdTaxEntity]
-    amount_clients <- invoices[, sum(Currency), by = by = IdTaxEntity]
+get_outcome <- function(invoices){
+    ## Per Client
+    c.income <- invoices[, mean(TotalAmount), by = c("IdTaxEntity",
+                                                   "IdClient")]
+    c.income <- c.income[, max(V1), by = IdTaxEntity]
+    names(c.income) <- c("IdTaxEntity", "max_mean_client")
+    ## Per day
+    d.income <- invoices[, mean(TotalAmount), by = c("IdTaxEntity",
+                                                   "PaymentDate")]
+    d.income <- d.income[, max(V1), by = IdTaxEntity]
+    names(d.income) <- c("IdTaxEntity", "max_mean_day")
+    ## Per month
+    invoices$month <- month(invoices$PaymentDate)
+    m.income <- invoices[, mean(TotalAmount), by = c("IdTaxEntity",
+                                                   "month")]
+    m.income <- m.income[, max(V1), by = IdTaxEntity]
+    names(m.income) <- c("IdTaxEntity", "max_mean_month")
+    ## Per year
+    invoices$year  <- year(invoices$PaymentDate)
+    y.income <- invoices[, mean(TotalAmount), by = c("IdTaxEntity",
+                                                   "year")]
+    y.income <- y.income[, max(V1), by = IdTaxEntity]
+    names(y.income) <- c("IdTaxEntity", "max_mean_year")
+    ## Merge together
+    outcome <- merge(c.income, d.income, by = "IdTaxEntity")
+    outcome <- merge(outcome, m.income, by = "IdTaxEntity")
+    outcome <- merge(outcome, y.income, by = "IdTaxEntity")
+    outcome
 }
 
-get_providers <- function(providers, invoices){
-    clients[, .N, by = IdTaxEntity]
+get_providers <- function(providers){
+    n_providers <- providers[, .N, by = 'IdTaxEntity']
+    n_providers
 }
 
-
+get_employees <- function(payrolls){
+    n_employees <- payrolls[, .N, by = IdTaxEntity]
+    n_employees
+}
 ## ------------------------------
 ## Read in data
 ## ------------------------------
-invoices   <- fread('../data/clean_data/df_invoices.csv')
-providers  <- fread('../data/export_V_Providers.csv')
-clients    <- fread('../data/export_v_clients.csv')
+invoices <- fread('../data/final_clean/df_invoices.csv')
+payrolls <- fread('../data/final_clean/df_payrolls.csv')
+received <- fread('../data/final_clean/df_received.csv')
